e655a1b4aab43b321f11b81582bcbc0fb144d49f
Karan <karandhongade2001@gmail.com>
2024-03-12 23:32:40 -0300

    errors fix

--- src/components/DishesCard.js
+++ src/components/DishesCard.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import {BsStarFill, BsStarHalf} from 'react-icons/bs';
-import Button from './Button';
+
 const DishesCard = (props) => {
     return (
         <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <a className="block relative h-48 rounded overflow-hidden">
+            <a href='/' className="block relative h-48 rounded overflow-hidden">
                 <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"></img>
             </a>
             <div className="mt-4">
