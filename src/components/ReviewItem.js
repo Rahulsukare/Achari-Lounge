0723cd026c67a8f043d8b67d3bbeea71a2034aad
Karan <karandhongade2001@gmail.com>
2024-03-13 13:50:56 -0300

    Review Form Added

--- src/components/ReviewItem.js
+++ src/components/ReviewItem.js
@@ -1,5 +1,5 @@
-import { BiSolidStar, BiSolidStarHalf } from 'react-icons/bi'
 import person from '../Assets/person.jpg'
+import Stars from './Stars'
 
 const ReviewItem = () => {
     return (
@@ -10,16 +10,10 @@ const ReviewItem = () => {
                     <img src={person} alt="profileImg" />
                 </div>
 
-                <div className='md:w-3/4 w-full px-4 py-6 border border-slate-300'>{/*Box */}
+                <div className='md:w-3/4 w-full px-8 py-6 border-l-8 border-l-green-600'>{/*Box */}
                     <div className='flex justify-between items-center'>
                         <h5 className='font-bold text-lg'>MIKLOS SALSA</h5>
-                        <div className=' text-amber-500 flex mb-2 items-center'>
-                            <BiSolidStar />
-                            <BiSolidStar />
-                            <BiSolidStar />
-                            <BiSolidStar />
-                            <BiSolidStarHalf />
-                        </div>
+                        <Stars/>
                     </div>
                     <p className=' text-md font-light mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quia, at dolor autem et nostrum voluptas doloribus? Magni ab voluptatem quos alias sunt debitis porro.</p>
                 </div>
