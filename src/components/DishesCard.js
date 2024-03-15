57ee22c3c8102cefa37574e4ba933b44232e5e02
Karan <karandhongade2001@gmail.com>
2024-03-15 01:14:52 -0300

    Navigate to home after login added

--- src/components/DishesCard.js
+++ src/components/DishesCard.js
@@ -5,9 +5,9 @@ import burgerImg from '../Assets/burger.png'
 const DishesCard = (props) => {
     return (
         <Link to='/item' className="lg:w-1/4 md:w-1/2 p-4 w-full hover:bg-amber-400 rounded-lg">
-            <a href='/' className="block relative h-48 rounded overflow-hidden">
+            <div className="block relative h-48 rounded overflow-hidden">
                 <img alt="ecommerce" className="object-cover object-center w-full h-full block" src={burgerImg}></img>
-            </a>
+            </div>
             <div className="mt-4">
                 <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
                 <h2 className="text-gray-900 title-font text-lg font-medium">{props.title}</h2>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,10 @@
-import React, {useRef, useState} from "react";
-import {checkValidData} from "../utils/Validate";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
-import {auth} from "../utils/firebase";
+import React, { useRef, useState } from "react";
+import { checkValidData } from "../utils/Validate";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router";
 const Login = () => {
+    const navigate = useNavigate();
     const [isSignInForm, setIsSignInForm] = useState(true);
     const [errorMessage, setErrorMessage] = useState(false);
     const name = useRef(null);
@@ -20,85 +22,88 @@ const Login = () => {
         console.log(password.current.value);
         console.log(msg);
 
-        if(msg)
+        if (msg)
             return;
         //sign In/ Sign Up
-        if(isSignInForm === false){
+        if (isSignInForm === false) {
             // Sign Up Logic
             createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
-            .then((userCredential) => {
-                // Signed up 
-                const user = userCredential.user;
-                console.log(user);
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                setErrorMessage(errorMessage);
-            });
+                .then((userCredential) => {
+                    // Signed up 
+                    const user = userCredential.user;
+                    console.log(user);
+                    console.log('User Registration Successfull')
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setErrorMessage(errorMessage);
+                });
         }
-        else{
+        else {
             //Sign In Logic
             signInWithEmailAndPassword(auth, email.current.value, password.current.value)
-            .then((userCredential) => {
-                // Signed in 
-                const user = userCredential.user;
-                console.log(user);
-                // ...
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                setErrorMessage(errorMessage);
-                
-            });
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    console.log(user);
+                    console.log("Log In Successfull")
+                    navigate('/home')
+                    // ...
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setErrorMessage(errorMessage);
+
+                });
         }
     }
     return (
         <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
-        <div className="md:w-1/3 max-w-sm">
-            <img className="rounded-full lg:w-96 md:w-48 sm: w-48"
-            src="../Assets/RestLogo.png"
-            alt="loginImg"
-            />
-        </div>
-        <div className="md:w-1/3 max-w-sm">
-            <div className="text-[#F4511F] my-5 text-center font-bold text-3xl md:text-left">
-                {(isSignInForm) ? "Sign In" : "Sign Up"}
+            <div className="md:w-1/3 max-w-sm">
+                <img className="rounded-full lg:w-96 md:w-48 sm: w-48"
+                    src="../Assets/RestLogo.png"
+                    alt="loginImg"
+                />
             </div>
-            <form action=""  onSubmit={(e) => e.preventDefault()} className=''>
-                {(!errorMessage) ? "" : <p className='font-semibold text-red-600 py-3'>{errorMessage}</p>}
-                {(isSignInForm) ? <></> : <input ref = {name} className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded" type="text" placeholder='Name'/>}
-                <input ref = {email}
-                className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-                type="text"
-                placeholder="Email Address"
-                />
-                <input ref = {password}
-                className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-                type="password"
-                placeholder="Password"
-                />
-                <button className='my-3 px-6 py-1 border-2 border-amber-600 text-amber-600 hover:bg-amber-600 hover:text-white transition-all rounded-full' onClick={handleButtonClick}>
+            <div className="md:w-1/3 max-w-sm">
+                <div className="text-[#F4511F] my-5 text-center font-bold text-3xl md:text-left">
                     {(isSignInForm) ? "Sign In" : "Sign Up"}
-                </button>
-            </form>
-            <div className="mt-4 flex justify-between font-semibold text-sm">
-            <label className="flex text-slate-500 hover:text-slate-600 cursor-pointer">
-                <input className="mr-1" type="checkbox" />
-                <span>Remember Me</span>
-            </label>
-            <a
-                className="text-[#F4511F] hover:text-[#ba2f05] hover:underline hover:underline-offset-4"
-                href="#"
-            >
-                Forgot Password?
-            </a>
+                </div>
+                <form action="" onSubmit={(e) => e.preventDefault()} className=''>
+                    {(!errorMessage) ? "" : <p className='font-semibold text-red-600 py-3'>{errorMessage}</p>}
+                    {(isSignInForm) ? <></> : <input ref={name} className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded" type="text" placeholder='Name' />}
+                    <input ref={email}
+                        className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
+                        type="text"
+                        placeholder="Email Address"
+                    />
+                    <input ref={password}
+                        className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <button className='my-3 px-6 py-1 border-2 border-amber-600 text-amber-600 hover:bg-amber-600 hover:text-white transition-all rounded-full' onClick={handleButtonClick}>
+                        {(isSignInForm) ? "Sign In" : "Sign Up"}
+                    </button>
+                </form>
+                <div className="mt-4 flex justify-between font-semibold text-sm">
+                    <label className="flex text-slate-500 hover:text-slate-600 cursor-pointer">
+                        <input className="mr-1" type="checkbox" />
+                        <span>Remember Me</span>
+                    </label>
+                    <a
+                        className="text-[#F4511F] hover:text-[#ba2f05] hover:underline hover:underline-offset-4"
+                        href="#"
+                    >
+                        Forgot Password?
+                    </a>
+                </div>
+                <div>
+                    <p className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left hover: cursor-pointer" onClick={toggleSignIn}>{(isSignInForm) ? "New to Achari-Lounge? Sign Up Now!" : "Already Registered? Sign In Now!"}</p>
+                </div>
             </div>
-            <div>
-                <p className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left hover: cursor-pointer" onClick={toggleSignIn}>{(isSignInForm) ? "New to Achari-Lounge? Sign Up Now!" : "Already Registered? Sign In Now!"}</p>
-            </div>
-        </div>
         </section>
     );
 };
